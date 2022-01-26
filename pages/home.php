782a44d55f57ee22414d1784871b977920e846f8
Lyz03 <lizoe@netcourrier.com>
2022-01-26 10:14:20 +0100

    home

--- pages/home.php
+++ pages/home.php
@@ -1,3 +1,9 @@
 <section>
     <h1>HOME</h1>
+
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi pariatur repellat tempore? Accusantium
+        adipisci blanditiis cumque enim eos eum, expedita, impedit maiores neque quisquam, saepe sequi veritatis vitae
+        voluptas voluptatum.
+    </p>
 </section>
