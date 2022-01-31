42c0cb14c18eefe2be6b97a59975992a76f53703
Lyz03 <lizoe@netcourrier.com>
2022-01-31 14:39:38 +0100

    save all users messages

--- public/save.php
+++ public/save.php
@@ -4,6 +4,9 @@ if (isset($_POST['submit'])) {
     $username = trim(strip_tags($_POST['username']));
     $userMessage = trim(strip_tags(($_POST['userMessage'])));
 
+    // save all messages
+    file_put_contents('../data/usersMessages.txt', $username . ', ' . $userMessage . "\n", FILE_APPEND);
+
     $array = [
         "username" => $username,
         "userMessage" => $userMessage,
@@ -11,16 +14,17 @@ if (isset($_POST['submit'])) {
 
     $array = json_encode($array);
 
+    // save the last message
     $jsonMessage = file_put_contents("../data/last_message.json", $array);
     json_encode($jsonMessage);
 
     // send a mail with the username and content
     $to = "xxxx@xxxxx.xxx";
     $subject = "New message from " . $username;
-    $message = $userMessage;
-    $from = 'From: xxxx@xxxxx.xxx';
+    $message = wordwrap($userMessage, 70, "\r\n");
+    $headers = 'From: xxxx@xxxxx.xxx';
 
-    if (mail($to, $subject, $message, $from)) {
+    if (mail($to, $subject, $message, $headers)) {
         header('Location: index.php?page=contact&m=0');
     } else {
         header('Location: index.php?page=contact&m=1');
