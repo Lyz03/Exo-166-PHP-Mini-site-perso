dd3a97d92230ebafc0bf1aeb27d240afdc0ddbeb
Lyz03 <lizoe@netcourrier.com>
2022-01-31 13:19:15 +0100

    basic admin page

--- public/admin.php
+++ public/admin.php
@@ -1,10 +1,29 @@
 <?php
 
+
+// on inclue le fichier qui contient nos fonctions
+require __DIR__ . '/../lib/functions.php';
+
+// l'exemple avec le header, à vous de jouer pour le reste
+getPart('header');
+
+getContent();
+
+echo '<button id="showLastMessage">voir le dernier message</button>';
+
 $content = file_get_contents(__DIR__ . '/../data/last_message.json');
 
 $content = json_decode($content, true);
 
+echo '<div id="lastMessage">';
+echo '<p>Last message</p>';
 foreach ($content as $key => $value) {
-    echo $key . ' : ' . $value . '<br>';
+    echo '<span>' . $key . ' : ' . $value . '</span><br>';
 }
+echo '</div>';
+
+getPart('footer');
+
+
+
 
--- public/assets/app.js
+++ public/assets/app.js
@@ -1,1 +1,15 @@
-console.log('js loaded');
+const lastMessageButton = document.querySelector('#showLastMessage');
+const lastMessageDiv = document.querySelector('#lastMessage');
+let a = 0;
+
+if (lastMessageButton) {
+    lastMessageButton.addEventListener('click', function () {
+        if (a === 0) {
+            lastMessageDiv.style.display = 'block';
+            a++
+        } else if (a === 1) {
+            lastMessageDiv.style.display = 'none';
+            a--
+        }
+    })
+}
